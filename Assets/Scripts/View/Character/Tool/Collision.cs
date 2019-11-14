8281ab0514d5bcc958c0069aeae993bdbd540687
Thomas Neumann <th.neumann.at@gmail.com>
2019-11-14 16:12:38 +0100

    Music Volume Upadte
    Penis

--- Assets/Scripts/View/Character/Tool/Collision.cs
+++ Assets/Scripts/View/Character/Tool/Collision.cs
@@ -11,18 +11,7 @@ namespace View.Character.Tool
         public GameObject menuCube;
         public GameObject hand;
         public Animator anim;
-    
-        // Start is called before the first frame update
-        void Start()
-        {
-        }
 
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
-    
         void OnCollisionEnter(UnityEngine.Collision col){
             if (col.gameObject.name == "RubiksCube")
             {
