7e6551da7287887bfc6ffbc49c82468ab4437a3b
Thomas Neumann <thomas.neumann@axtesys.at>
2019-11-14 11:39:15 +0000

    model cube collision verbesserung
    penis

--- Assets/Scripts/View/UI/animation/CubeController.cs
+++ Assets/Scripts/View/UI/animation/CubeController.cs
@@ -8,49 +8,26 @@ namespace View.UI.animation
 {
     public class CubeController : MonoBehaviour
     {
-
         private Animator _anim;
         private bool _open = false;
 
-        // Start is called before the first frame update
         private void Start()
         {
             _anim = GetComponent<Animator>();
         }
 
-        // Update is called once per frame
-        private void FixedUpdate()
+        public void SwitchMenuState()
         {
-            if (OVRInput.GetDown(OVRInput.Button.One))
+            if (_open)
             {
-                if (_open)
-                {
-                    _anim.SetTrigger("Close");
-                    _open = !_open;
-                }
-                else
-                {
-                    _anim.SetTrigger("Open");
-                    _open = !_open;
-                }
+                _anim.SetTrigger("Close");
+                _open = !_open;
             }
-            else if (Input.GetKeyDown("space"))
+            else
             {
-                if (_open)
-                {
-                    _anim.SetTrigger("Close");
-                    _open = !_open;
-                }
-                else
-                {
-                    _anim.SetTrigger("Open");
-                    _open = !_open;
-                }
-
-
+                _anim.SetTrigger("Open");
+                _open = !_open;
             }
         }
     }
-
 }
-
