e6ed8797ce4c614dee73a01ec3f0ae4189150709
Thomas Neumann <th.neumann.at@gmail.com>
2019-11-14 11:30:40 +0100

    collision rubixcube

---
+++ Assets/Scripts/View/Character/Tool/Collision.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace View.Character.Tool
+{
+    public class Collision : MonoBehaviour
+    {
+    
+        public GameObject model;
+        public GameObject menuCube;
+        public GameObject hand;
+    
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    
+        void OnCollisionEnter(UnityEngine.Collision col){
+            if (col.gameObject.name == "RubiksCube")
+            {
+                model.SetActive(false);
+                Destroy(col.gameObject);
+                menuCube.SetActive(true);
+                hand.SetActive(false);
+            }
+        }
+    
+    }
+
+}
+
