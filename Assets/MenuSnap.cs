1866fc5939e8a78a20215002708cd08d44e4088d
AlymAlym <alymalym@web.de>
2020-01-14 08:54:41 +0100

    StartScreen Menu

--- Assets/MenuSnap.cs
+++ Assets/MenuSnap.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -7,7 +8,22 @@ public class MenuSnap : MonoBehaviour
 {
     
     public UnityEvent triggered = new UnityEvent();
-    public GameObject letter; 
+    public GameObject letter;
+    public GameObject placeholderStart;
+    public Vector3 letterTransformation;
+    public Quaternion letterRotation;
+    public Quaternion startRotation;
+    public Vector3 startTransformation;
+    
+    private void Start()
+    {
+        letter = GameObject.Find("LetterT");
+        placeholderStart = GameObject.Find("Placeholder_Start");
+        letterTransformation = transform.position;
+        startTransformation = placeholderStart.transform.position;
+        startRotation = placeholderStart.transform.rotation;
+    }
+
     void OnTriggerEnter(Collider collider)
     {
         //Check for a match with the specified name on any GameObject that collides with your GameObject
@@ -15,13 +31,12 @@ public class MenuSnap : MonoBehaviour
         {
             //If the GameObject's name matches the one you suggest, output this message in the console
             Debug.Log("Start Menu");
-            letter = collider.gameObject;
-            letter.transform.position = new Vector3(0.2f, 2.255f, 1.36f);
+            //letter = collider.gameObject;
+            transform.position = startTransformation;
+            transform.rotation = startRotation;
             //letter.transform.rotation = Quaternion.Euler(new Vector3(90,0,0));
-            transform.rotation = Quaternion.Euler(90, 0, 0);
+            //transform.rotation = Quaternion.Euler(90, 0, 0);
             triggered.Invoke();
         }
-
-        
     }
 }
